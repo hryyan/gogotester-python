901d0a3fec1097e85cdbf5a2fbc785ba2bb11731
vincent19376 <vincent19376@gmail.com>
2015-09-04 23:08:26 +0800

    Done!

--- src/config.py
+++ src/config.py
@@ -8,12 +8,10 @@ def read_config(cfg_file=None):
     cfg = configparser.ConfigParser()
 
     if cfg_file is None:
-        cfg["DEFAULT"] = {"ipv4_limit" : 20,
-                          "ipv6_limit" : 20,
-                          "socket_per_thread": 50,
-                          "ssl_per_thread": 50,
-                          "timeout": 1,
-                          "socket_timeout": 10,
+        cfg["DEFAULT"] = {"limit" : 20,
+                          "socket_num": 50,
+                          "ssl_num": 50,
+                          "socket_timeout": 1,
                           "ssl_timeout": 10}
         logging.info("Don't assign config, use default.")
     else:
@@ -22,13 +20,11 @@ def read_config(cfg_file=None):
 
     c = cfg["DEFAULT"]
 
-    logging.info("ipv4_limit = %s, ipv6_limit = %s"
-                 % (c["ipv4_limit"], c["ipv6_limit"]))
-    logging.info("socket_per_thread = %s, ssl_per_thread = %s"
-                 % (c["socket_per_thread"], c["ssl_per_thread"]))
+    logging.info("limit = %s" % c["limit"])
+    logging.info("socket_num = %s, ssl_num = %s"
+                 % (c["socket_num"], c["ssl_num"]))
     logging.info("socket_timeout = %s, ssl_timeout = %s"
                  % (c["socket_timeout"], c["ssl_timeout"]))
-    logging.info("timeout = %s" % c["timeout"])
 
     return cfg
 
